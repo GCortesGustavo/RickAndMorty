98d783a2a2918062f6333b6d9d25b9f7b517cf02
GCortesGustavo <gustavocortes934@gmail.com>
2023-10-28 18:18:00 -0300

    Proyecto Finalizado

--- Client/src/App.js
+++ Client/src/App.js
@@ -55,7 +55,6 @@ function App() {
       }).catch((error) => {
          window.alert(error)
       })
-
    }
 
    return (
--- Server/src/controllers/getCharById.js
+++ Server/src/controllers/getCharById.js
@@ -1,35 +1,3 @@
-// const axios = require("axios"); //exportamos axios
-
-
-// const URL = "https://rickandmortyapi.com/api/character" //api
-
-// const getCharById = (req, res) => { //solicitud: res, respuesta : res
-//     const {id} = req.params; //se extrae el paramentro id con destructuring.
-    
-//     axios(`${URL}/${id}`) //Solicitud a la api para obtener informacion de un personaje especifico
-//     .then(response => response.data)
-//     .then(({status, name, species, origin, image, gender}) => { //destructuring del personaje
-//         if(name) { 
-//             const character = { //se crea el objeto que contiene la info del personaje
-//                 id, 
-//                 name,
-//                 species,
-//                 origin, 
-//                 image,
-//                 gender,
-//                 status
-//             }
-            
-//             return res.status(200).json(character); //se devuelve una respuestaOK con el objeto character como JSON
-//         }
-        
-//         return res.status(404).json("Not Found") //Si el nombre no se encuentra en la respuesta, se deuvelve un errror 404 con el mensaje "Not Found"
-//     })
-//     .catch((error) => res.status(500).json({message: error.message})) //Si se produce un error al obtener la respuesta de la Api se deuvelve un error 500 y el objto JSOPN tien un mensaje de error
-// }
-
-// module.exports = {getCharById} //se exporta la funcion para que pueda ser utilizada por otros modulos
-
 const axios = require('axios')
 const URL = "https://rickandmortyapi.com/api/character/"
 
